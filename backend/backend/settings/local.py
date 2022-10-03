6d8964e05ddab926c98aed42043fbb93cb8c4c07
kdh92417 <kdh92417@gmail.com>
2022-10-03 15:07:27 +0900

    :card_file_box: Chore: 로컬DB 설정

--- backend/backend/settings/local.py
+++ backend/backend/settings/local.py
@@ -3,3 +3,15 @@ from .base import *
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "zium",
+        "USER": "root",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": 3306,
+    }
+}
